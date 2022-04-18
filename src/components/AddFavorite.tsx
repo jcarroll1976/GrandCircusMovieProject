d226484865b9adaa5086e0e96ac919e137fb7b04
Jordan Salgado <JDSalgado41803@gmail.com>
2022-04-18 18:42:28 -0400

    added AddFavorite component

---
+++ src/components/AddFavorite.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './AddFavorite.css';
+
+const AddFavorite = () => {
+    return (
+        
+       <button className="FavoriteButton">Add to Favorites</button> 
+        
+    );
+};
+
+export default AddFavorite
