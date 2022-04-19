bbf8f47de1b7398515c0f52a0deb0fccc5d89362
Jordan Salgado <JDSalgado41803@gmail.com>
2022-04-18 21:49:11 -0400

    edit favorite list

--- src/components/FavoriteList.tsx
+++ src/components/FavoriteList.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import './FavoriteList.css';
+import MovieContext from "../context/MovieContext";
 
-const FavoriteList = () => {
+function FavoriteList() {
+    const { favoriteMovies } = useContext(MovieContext);
     return (
-       <div className="FavoriteList">
-           <h1>Your favorite list</h1>
+       <div>
+           <h1>Your favorite movies</h1>
+           <ul>
+               {favoriteMovies.map((todo, i) =>
+               <li key={i}>{}</li>)}
+           </ul>
            <Link to={"/"}>To go back to main menu</Link>
        </div>
     );
