f9f3cef703d9c70ad54ddb592024e07d86311a08
Claire Utrecht <utrechtc@gmail.com>
2022-04-20 19:46:02 -0400

    created movie interface hopefully correctly :)

--- src/models/MovieInterface.ts
+++ src/models/MovieInterface.ts
@@ -1,7 +1,21 @@
 import React from "react";
 
 export default interface Movie {
-    title: string;
+    original_title: string;
+    id: number;
+    vote_average: number;
+    overview: string;
     runtime: number;
     genre: string;
+    poster_path: string;
+    genre_ids: GenreResults[];
+}
+
+
+export interface APIResponse {
+    results: Movie[];
+}
+
+export interface GenreResults {
+    genre_ids: number[];
 }
