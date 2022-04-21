7c41422eb9b4b972a0a311c2b6cde13a82769763
Jordan Salgado <JDSalgado41803@gmail.com>
2022-04-21 19:10:16 -0400

    added buttons

--- src/components/MovieComponent.tsx
+++ src/components/MovieComponent.tsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import Movie from '../models/MovieInterface';
 import MovieContext from "../context/MovieContext";
 import { fetchMovies } from "../services/MovieService";
+import AddFavorites from "./AddFavorites";
+interface Props {
+    movie: Movie;
+}
 
-export default function Movie() {
- const [movies, setMovies] = useState<Movie[]>([]);
-
- useEffect(() => {
-     fetchMovies().then(data => {
-         setMovies(data);
-     });
- }, []);
+export default function SingleMovie({movie}: Props) {
+const [favorites, setFavorites] =useState([]);
+ return (
+    <div className = "Movie">
+        <img src={movie.poster_path}></img>
+        <AddFavorites />
+    </div>
+ )
 }
