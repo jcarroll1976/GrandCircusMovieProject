db35595a44c41b936221fa4078a1c1c876913cd5
Jordan Salgado <JDSalgado41803@gmail.com>
2022-04-20 20:54:58 -0400

    hi

--- src/components/MovieComponent.tsx
+++ src/components/MovieComponent.tsx
@@ -3,16 +3,12 @@ import Movie from '../models/MovieInterface';
 import MovieContext from "../context/MovieContext";
 import { fetchMovies } from "../services/MovieService";
 
-interface Props {
-    movie: Movie;
-}
+export default function Movie() {
+ const [movies, setMovies] = useState<Movie[]>([]);
 
-export default function SingleMovie({movie}: Props) {
-
- return (
-    <div className = "Movie">
-        //movie poster and title Header
-        <img src = {movie.poster_path}/>
-    </div>
- )
+ useEffect(() => {
+     fetchMovies().then(data => {
+         setMovies(data);
+     });
+ }, []);
 }
